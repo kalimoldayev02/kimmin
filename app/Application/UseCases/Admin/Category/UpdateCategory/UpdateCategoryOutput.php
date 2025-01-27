88a774278e61bb64a2a90eccd80f6d7c1fe700fe
kalimoldayev02 <kalimoldayev02@gmail.com>
2025-01-27 09:09:51 +0500

    fix(Review): Исправил замечания

--- app/Application/UseCases/Admin/Category/UpdateCategory/UpdateCategoryOutput.php
+++ app/Application/UseCases/Admin/Category/UpdateCategory/UpdateCategoryOutput.php
@@ -5,7 +5,7 @@ namespace App\Application\UseCases\Admin\Category\UpdateCategory;
 class UpdateCategoryOutput
 {
     public function __construct(
-        public int    $id,
+        public int $id,
     )
     {
     }
