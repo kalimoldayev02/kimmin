2fd31e7c7bd202f5bcceb29bf44db8da49456f33
kalimoldayev02 <kalimoldayev02@gmail.com>
2025-01-29 22:07:59 +0500

    refactor(Some changes): Исправил замечания

--- routes/api.php
+++ routes/api.php
@@ -5,23 +5,23 @@ use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\FileController;
 use App\Http\Controllers\Admin\CategoryController;
 
-Route::prefix('admin')->group(function() {
+Route::prefix('admin')->name('admin.')->group(function() {
     Route::controller(AuthController::class)->group(function() {
-        Route::post('/login', 'login')->name('admin.login');
-        Route::post('/logout', 'logout')->middleware(['auth:sanctum', 'role.admin'])->name('admin.logout');
+        Route::post('/login', 'login')->name('login');
+        Route::post('/logout', 'logout')->middleware(['auth:sanctum', 'role.admin'])->name('logout');
     });
 
     Route::middleware(['auth:sanctum', 'role.admin'])->group(function() {
         Route::prefix('category')->controller(CategoryController::class)->group(function() {
-            Route::get('/', 'getCategories')->name('admin.get_categories');
-            Route::post('/', 'createCategory')->name('admin.create_category');
-            Route::get('/{category}', 'getCategory')->name('admin.get_category');
-            Route::post('/{category}', 'updateCategory')->name('admin.update_category');
+            Route::get('/', 'getCategories')->name('getCategories');
+            Route::post('/', 'createCategory')->name('createCategory');
+            Route::get('/{category}', 'getCategory')->name('getCategory');
+            Route::post('/{category}', 'updateCategory')->name('updateCategory');
         });
 
         Route::prefix('file')->controller(FileController::class)->group(function() {
-            Route::post('/upload', 'upload')->name('admin.file_upload');
-            Route::post('/delete', 'delete')->name('admin.file_delete');
+            Route::post('/upload', 'upload')->name('uploadFile');
+            Route::post('/delete', 'delete')->name('deleteFile');
         });
     });
 });
